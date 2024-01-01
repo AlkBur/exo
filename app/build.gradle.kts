bea4588baaf1646b91052e3212725b116641d11f
BurA <alkboor@mail.ru>
2024-01-01 22:40:04 +0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,10 +37,10 @@ android {
 }
 
 dependencies {
-    implementation ("androidx.media3:media3-exoplayer:1.0.1")
-    implementation ("androidx.media3:media3-exoplayer-dash:1.0.1")
-    implementation ("androidx.media3:media3-ui:1.0.1")
-    implementation ("androidx.media3:media3-exoplayer-rtsp:1.2.0")
+    implementation ("com.google.android.exoplayer:exoplayer-core:2.19.1")
+    implementation ("com.google.android.exoplayer:exoplayer-dash:2.19.1")
+    implementation ("com.google.android.exoplayer:exoplayer-ui:2.19.1")
+    implementation ("com.google.android.exoplayer:exoplayer-rtsp:2.19.1")
 
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/ru/burlakov/exo/MainActivity.kt
+++ app/src/main/java/ru/burlakov/exo/MainActivity.kt
@@ -2,14 +2,17 @@ package ru.burlakov.exo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.media3.common.MediaItem
-import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.rtsp.RtspMediaSource
-import androidx.media3.exoplayer.source.MediaSource
-import androidx.media3.ui.PlayerView
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.rtsp.RtspMediaSource
+import com.google.android.exoplayer2.ui.StyledPlayerView
+
 
 class MainActivity : AppCompatActivity() {
-    lateinit var playerVew: PlayerView
+    lateinit var playerView: StyledPlayerView
     lateinit var player: ExoPlayer
     //val rtspUri = "https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4"
     val rtspUri = "rtsp://admin:<password>@<ip_address>:554/streaming/channels/1"
@@ -17,10 +20,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        playerVew = findViewById(R.id.player_view)
+        playerView = findViewById(R.id.player_view)
         // Create a player instance.
         val player = ExoPlayer.Builder(this).build()
-        playerVew.player = player
+        player.addListener(object : Player.Listener {
+            override fun onPlayerError(error: PlaybackException) {
+                // Re-initialize player at the current live window default position.
+                //player.seekToDefaultPosition()
+                player.prepare()
+                player.setPlayWhenReady(true);
+                //player.play()
+
+            }
+        })
+        playerView.player = player
 
         // Create an RTSP media source pointing to an RTSP uri.
         val mediaSource: MediaSource = RtspMediaSource.Factory()
@@ -32,7 +45,14 @@ class MainActivity : AppCompatActivity() {
         player.setMediaSource(mediaSource);
         // Prepare the player.
         player.prepare()
-        player.play()
+        player.setPlayWhenReady(true);
+        //player.play()
 
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        playerView.onPause();
+        player.release();
     }
 }
